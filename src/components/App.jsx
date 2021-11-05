e49bafb01949eaf13d2d824e35d00bda413422f5
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-11-06 00:55:28 +0700

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+
+  function textHandle(event) {
+    const textBox = event.target.value;
+    setTask(textBox);
+    console.log(task);
+  }
+
+  function buttonHandle() {}
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
+      <form action={buttonHandle}>
+        <div className="form">
+          <input type="text" onChange={textHandle} />
+          <button type="submit">
+            <span>Add</span>
+          </button>
+        </div>
+      </form>
       <div>
         <ul>
           <li>A Item </li>
